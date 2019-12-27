e12edfa7252dba065ddaa90836e3d008a0335449
Felipe Fett <felipe.fett@opngo.com.br>
2019-12-27 16:58:53 -0300

    Updates

---
+++ precache-manifest.54c45a3704769da29b2b9c7f28207362.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc2ee47c05d3ff3ba0837494364d9b9",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "53c49e9951be51fc9ff4",
+    "url": "/about/static/css/main.65da53ca.chunk.css"
+  },
+  {
+    "revision": "a7259c6852ff19efa95f",
+    "url": "/about/static/js/2.e8b3df13.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/about/static/js/2.e8b3df13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53c49e9951be51fc9ff4",
+    "url": "/about/static/js/main.b5b1edc9.chunk.js"
+  },
+  {
+    "revision": "b9080fa57f2b608e900f",
+    "url": "/about/static/js/runtime-main.17e4eaec.js"
+  },
+  {
+    "revision": "2823aff5ff968e83df9288ee83d8a9ce",
+    "url": "/about/static/media/alldaylong.2823aff5.jpg"
+  },
+  {
+    "revision": "56c8ef7f5333f94ffdd7a0e5ddcb4475",
+    "url": "/about/static/media/angle-down.56c8ef7f.svg"
+  },
+  {
+    "revision": "65e3d2e336ec62abf877c27acb2040c2",
+    "url": "/about/static/media/band.65e3d2e3.jpg"
+  },
+  {
+    "revision": "60b54ee87044af598a440a6396e5224a",
+    "url": "/about/static/media/bqc.60b54ee8.jpg"
+  },
+  {
+    "revision": "27843052211fe565b0997a59fc3f154f",
+    "url": "/about/static/media/cafe.27843052.jpg"
+  },
+  {
+    "revision": "d7049c6c4265e21257878a5cea77265f",
+    "url": "/about/static/media/codepen.d7049c6c.svg"
+  },
+  {
+    "revision": "47bfe40d24ed78036e3518156c81ef8b",
+    "url": "/about/static/media/craft.47bfe40d.jpg"
+  },
+  {
+    "revision": "5d42bd2967ffccec61452cde302bd2b4",
+    "url": "/about/static/media/eita.5d42bd29.jpg"
+  },
+  {
+    "revision": "6cad3fe6c721f81d289270946b697c16",
+    "url": "/about/static/media/facebook.6cad3fe6.svg"
+  },
+  {
+    "revision": "c09666fbe0e2db9d8ad6fc1d5ceaa1dc",
+    "url": "/about/static/media/family.c09666fb.jpg"
+  },
+  {
+    "revision": "93b8836ba3af4dc8c78211fcc102d87c",
+    "url": "/about/static/media/festival.93b8836b.jpg"
+  },
+  {
+    "revision": "9f994f8f7eda2599282d6af7d92e386a",
+    "url": "/about/static/media/fett.9f994f8f.png"
+  },
+  {
+    "revision": "8b21b782d659d659154eea0a669c78a2",
+    "url": "/about/static/media/fogo.8b21b782.gif"
+  },
+  {
+    "revision": "bb1b752c5d1bd79fc4a4480352efc3e7",
+    "url": "/about/static/media/food.bb1b752c.jpg"
+  },
+  {
+    "revision": "58dbdb42cf711031a871a721b7f937fa",
+    "url": "/about/static/media/fun.58dbdb42.jpg"
+  },
+  {
+    "revision": "e372a0f0525d15af9b059dac9f4eb0b6",
+    "url": "/about/static/media/github.e372a0f0.svg"
+  },
+  {
+    "revision": "430299d438e525b9c4648388fc32b8d1",
+    "url": "/about/static/media/grupols.430299d4.jpg"
+  },
+  {
+    "revision": "2f41a8b8f3914eaa9341cff2767f5422",
+    "url": "/about/static/media/instagram.2f41a8b8.svg"
+  },
+  {
+    "revision": "e1ca6fec2925c167342b4bc42297289b",
+    "url": "/about/static/media/lasier.e1ca6fec.gif"
+  },
+  {
+    "revision": "5b72957e67507f10bb1e1d66e09f1f94",
+    "url": "/about/static/media/linkedin.5b72957e.svg"
+  },
+  {
+    "revision": "7af39fc677698b3a147af1de432a18de",
+    "url": "/about/static/media/morebeer.7af39fc6.jpg"
+  },
+  {
+    "revision": "16bbd6c22df513c6d25f006cdfd96685",
+    "url": "/about/static/media/movies.16bbd6c2.jpg"
+  },
+  {
+    "revision": "c393ad3db68faa9344d52874ee9fc276",
+    "url": "/about/static/media/numsei.c393ad3d.jpg"
+  },
+  {
+    "revision": "4d803713d00e7de91755fdce35906861",
+    "url": "/about/static/media/santana.4d803713.gif"
+  },
+  {
+    "revision": "1896d4dd02754ebe0c8da0e9cc53e35c",
+    "url": "/about/static/media/shows.1896d4dd.jpg"
+  },
+  {
+    "revision": "dae9d5beda4db4a4da61e10bac0745f8",
+    "url": "/about/static/media/stefanini.dae9d5be.jpg"
+  },
+  {
+    "revision": "c9f462f55192c64cfb83d0261ba8e2d0",
+    "url": "/about/static/media/vg8.c9f462f5.jpg"
+  }
+]);
