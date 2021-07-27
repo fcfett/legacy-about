8cc73e9228b19c13ce9a24e6cce19c0593200365
Felipe Fett <felipe.fett@hypeflame.com.br>
2021-07-27 11:34:57 -0300

    Updates Quote slide

--- src/pages/Quote/index.jsx
+++ src/pages/Quote/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './style.scss';
 
+import cap from '../../assets/images/cap.png';
 import fett from '../../assets/images/fett.png';
+import glasses from '../../assets/images/glasses.png';
 
 export default () => (
   <div id="page-4" className="page font-serif">
@@ -9,12 +11,16 @@ export default () => (
       <q>
         Senioridade, acima de tudo, é uma questão de postura.
         <br />
-        Os problemas não deixarão de aparecer por conta do teu currículo espetacular.
+        De nada vale um currículo todo enfeitadinho se na hora do pega tu não assume teus BO.
       </q>
       <strong>Felipe Fett</strong>
     </article>
     <aside>
-      <img src={fett} alt="Felipe Fett" />
+      <figure>
+        <img className="fett" src={fett} alt="Felipe Fett" />
+        <img className="glasses" src={glasses} alt="Thug Life Glasses" />
+        <img className="cap" src={cap} alt="Thug Life Cap" />
+      </figure>
     </aside>
   </div>
 );
