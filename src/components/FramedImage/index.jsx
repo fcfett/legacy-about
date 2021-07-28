4fae76b0b1b58773f28aa1d111d7b94c37e3f1f7
Felipe Fett <felipe.fett@hypeflame.com.br>
2021-07-27 21:55:31 -0300

    Updates figure rendering strategy

--- src/components/FramedImage/index.jsx
+++ src/components/FramedImage/index.jsx
@@ -1,11 +1,13 @@
-import React, { Suspense } from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 
-export default ({ className, src, alt, caption }) => (
-  <Suspense>
-    <figure className={`image ${className}`}>
-      <img src={src} alt={caption || alt} />
+export default ({ className, src, alt, caption }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <figure className={`image ${className} ${loaded ? 'visible' : ''}`}>
+      <img src={src} alt={caption || alt} onLoad={() => setLoaded(true)} />
       {caption && <figcaption>{caption}</figcaption>}
     </figure>
-  </Suspense>
-);
+  );
+};
